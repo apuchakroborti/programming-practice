55abb64f75267edc382569faf3a9258dd7af3eda
Apu Kumar Chakroborti <apu.chakroborti@tigerit.com>
2022-03-23 06:02:58 +0600

    first commit

---
+++ src/codilitypractice/ThreeLetters.java
@@ -0,0 +1,32 @@
+package codilitypractice;
+
+public class ThreeLetters {
+    public String solution(int A, int B){
+        char c1='a', c2='b';
+        int i=A, j=B;
+        if(i<j){
+            i=B;
+            j=A;
+            c1='b';
+            c2='a';
+        }
+        StringBuilder builder= new StringBuilder(A+B);
+        while(i>0){
+            //start with the bigger value
+            builder.append(c1);
+            i--;
+            if(i>j){
+                builder.append(c1);
+                i--;
+            }
+            if(j>0){
+                builder.append(c2);
+                j--;
+            }
+            
+        }
+        
+        return builder.toString();
+    }
+    
+}
